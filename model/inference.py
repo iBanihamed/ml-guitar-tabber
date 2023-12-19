49ebe7079a77a0f0848d51ad9efbc16cb8939630
vn53q3k <vn53q3k@homeoffice.wal-mart.com>
2023-12-18 22:57:23 -0600

    changing inference to use dictionary instead of conditionals

--- model/inference.py
+++ model/inference.py
@@ -1,7 +1,7 @@
 from __future__ import division
 import asyncio
 import os
-from progress.bar import Bar
+# from progress.bar import Bar
 import numpy as np
 
 from model.model import Net
@@ -21,6 +21,12 @@ class ModelInferencer:
         self.transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)), transforms.Resize((224, 224))])  # Same as for your validation data, e.g. Resize, ToTensor, Normalize, ...
+        self.chords = {
+            0: 'major', 
+            1: 'major7',
+            2: 'minor', 
+            3: 'thumb'
+        }
 
     def convertImage(self, imgPath):
         img = Image.open(imgPath).convert("RGB")
@@ -30,22 +36,11 @@ class ModelInferencer:
 
     def predict(self, img):
         output = self.model(img)
-        #print(output)
         pred = torch.argmax(output, 1)
         return pred
 
     def classifyPrediction(self, prediction):
-        if prediction.item() == 0:
-            predClass = "major"
-        elif prediction.item() == 1:
-            predClass = "major7"
-        elif prediction.item() == 2:
-            predClass = "minor"
-        elif prediction.item() == 3:
-            predClass = "minor7"
-        else:
-            predClass = "NODATA"
-        return predClass
+        return self.chords[prediction.item()]
 
     # def loadModel(self, modelSize, mapLocation='cpu'):
     #     self.model = Net(modelSize)
