ad7a61daf426ca04c121223d78cca320e40a9459
vn53q3k <vn53q3k@homeoffice.wal-mart.com>
2024-02-28 16:47:14 -0600

    getting rid of old code

--- chordDetector.py
+++ chordDetector.py
@@ -1,43 +1,10 @@
 from data_capture.object_detector.handDetector import HandWatcher
-from data_capture.note_detector.detectFrequency import FrequencyDetector
+from data_capture.object_detector.neckDetect import LineDetector
+# from data_capture.note_detector.detectFrequency import FrequencyDetector
 
-freqDetect = FrequencyDetector()
+# freqDetect = FrequencyDetector()
+neckDetect = LineDetector()
 chordDetect = HandWatcher()
 
-videoPath = "./data_capture/videos/major7.MOV" # video directory for saved vids, 0 for input to be live webcam feed
-# input_path = './data_capture/videos/standByMeChordsTrimmed.mp4'
-output_path = './data_capture/note_detector/audio_files/audioCut.wav'
-new_audio = './data_capture/note_detector/audio_files/audioFile.wav'
-
-# Extract audio from video file and convert to mono
-freqDetect.extractAudio(videoPath, output_path)
-freqDetect.convertMono(output_path, new_audio)
-frequencies = freqDetect.getFreq(new_audio)
-#print(frequencies)
-
-# Ismael to create and call on new method in HandWatcher to create list of chordshapes with their respective timestamps
-chordDetect.detectChordShapes(videoPath)
-chordShapes = chordDetect.getChordShapes() #list of chords
-#print(chordShapes)
-
-# setting length of chordList 
-if(len(frequencies) < len(chordShapes)):
-    length = len(frequencies)
-else:
-    length = len(chordShapes)
-
-# Jorge to create method for corelating list and merging them into one list 
-# Corelate frequency and chordShape list
-chordList = []
-
-for i in range(length):
-    chordList.append(frequencies[i] + chordShapes[i])
-
-print("Final List: ")
-print(chordList)
-
-# Ismael to take third list output and write it to the vidfile at the respective frames  
-
-
-##Logic to match the chordshapes with their respective frequencies at their respective frames based on the timestamps
+chordDetect.detectChordShapes(0) # Use 0 for iphone camera 
 
