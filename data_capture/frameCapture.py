e4fc8bbb7db21e5772f8c8e1e8c24379626eccd7
vn53q3k <vn53q3k@homeoffice.wal-mart.com>
2024-02-28 16:50:41 -0600

    changing name of file

--- data_capture/frameCapture.py
+++ data_capture/frameCapture.py
@@ -1,6 +1,8 @@
 import cv2
 import os
 import re
+from object_detector.neckDetect import LineDetector
+from object_detector.handDetectAdv import HandDetect
 
 guitar_vids= "./data_capture/videos"
 
@@ -19,7 +21,7 @@ for video in os.listdir(guitar_vids):
     count = 0 
     while success:
         # Write every successful captured frame to a file in framespath
-        cv2.imwrite(f"{framesPath}/{video}frame{count}.jpg", image)
+        cv2.imwrite(f"{framesPath}/{video}frame{count}.jpg", LineDetector().run(HandDetect().cropHand(image)))
         success, image = vidcap.read()
         print(str(vidcap.get(cv2.CAP_PROP_POS_MSEC)))
         print(f'Read frame {count}: ', success)
